9cb6a2974adc774fc06762419954d682a4ad582e
Biza_13 <serrano_0_1@hotmail.com>
2025-01-02 15:47:37 +0100

    pequeñas modificaciones

--- pruebaB/backend.tf
+++ pruebaB/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "cubo-s3-begona"
-    key            = "state/terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
   }
 }
